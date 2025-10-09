82998fdcbc9369430f312d924af7c97d128f58ba
LikeNo1Else <socratez.hf@gmail.com>
2025-10-09 00:39:53 +0000

    Added credt to jenkins

--- 1.groovy
+++ 1.groovy
@@ -1,8 +1,48 @@
-node {
-    stage("Checkout SCM") {
-        git branch: 'main', url: 'https://github.com/LikeNo1Else/flask-app-build.git'
+template = '''
+apiVersion: v1
+kind: Pod
+metadata:
+  labels:
+    run: docker
+  name: docker
+spec:
+  containers:
+  - command:
+    - sleep
+    - "3600"
+    image: docker
+    name: docker
+    volumeMounts:
+    - mountPath: /var/run/docker.sock
+      name: docker
+  volumes:
+  - name: docker
+    hostPath:
+      path: /var/run/docker.sock
+    '''
+
+podTemplate(cloud: 'kubernetes', label: 'docker', yaml: template) {
+node("docker") {
+    container("docker") {
+    stage("Checkout SCM") { 
+        git branch: 'main', url: 'https://github.com/liken01else/flask-app-build.git'
+    } 
+
+
+    withCredentials([usernamePassword(credentialsId: 'docker-creds', passwordVariable: 'DOCKER_PASS', usernameVariable: 'DOCKER_USER')]) {
+   
+    stage("Docker Hub logging") {
+        sh "docker login -u ${DOCKER_USER} -p ${DOCKER_PASS}"
     }
-    stage("Check") {
-        sh "ls"
+
+    stage("Docker build") {
+        sh "docker build -t ${DOCKER_USER}/jenkins-flask-app:1.0.0 ."
     }
+   
+    stage("Docker push") {
+        sh "docker push ${DOCKER_USER}/jenkins-flask-app:1.0.0"
+    }
+   }
+  }
+ }
 }
